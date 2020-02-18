8e9bd0e88fc5c9c4d066ee824a6c7854849a5cd2
Sukanya <47360797+sukanyashete@users.noreply.github.com>
2020-02-18 17:59:03 +0530

    Add files via upload

---
+++ string_palindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+	char str[20];
+	int i=0,j=0;
+	cout<<"\nEnter a string";
+	cin>>str;
+	while(str[i]!='\0'){ i++; }
+	while(j<i){
+		if(str[i-1]!= str[j]){
+			cout<<"\nString is not a palindrome";
+			exit(0);
+			}
+		j++;
+		i--;
+		}
+	cout<<"\nString is a palindrome";
+	}
